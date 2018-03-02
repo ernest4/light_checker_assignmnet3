1d2091e135f4b7e1509d7f509b922865316e4147
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2018-03-02 20:59:58 +0000

    Using numpy.array should yield a performance boost over python standard lists

--- light_counter/lightTester.py
+++ light_counter/lightTester.py
@@ -4,6 +4,8 @@ Created on 28 Feb 2018
 @author: ernest
 '''
 
+import numpy as np
+
 class LightTester:
     """
     This class is a container for two dimensional light grid with methods to modify the grid and return the element count.
@@ -11,7 +13,8 @@ class LightTester:
     lights = None #!!! PUBLIC for testing purposes, make PRIVATE once tests are done !!!
     
     def __init__(self, size):
-        self.lights = [[False]*size for _ in range(size)]
+        #self.lights = [[False]*size for _ in range(size)]
+        self.lights = np.array([[False]*size for _ in range(size)])
         
     def apply(self, command):
         action, x1, y1, x2, y2 = command[0], command[1], command[2], command[3], command[4]
@@ -20,25 +23,25 @@ class LightTester:
             for row in range(len(self.lights)):
                 for col in range(len(self.lights[row])):
                     if y2 >= row >= y1 and x2 >= col >= x1:
-                        self.lights[row][col] = True
+                        self.lights[row,col] = True
         elif command[0] == "turn off":
             for row in range(len(self.lights)):
                 for col in range(len(self.lights[row])):
                     if y2 >= row >= y1 and x2 >= col >= x1:
-                        self.lights[row][col] = False
+                        self.lights[row,col] = False
         else: #switch
             for row in range(len(self.lights)):
                 for col in range(len(self.lights[row])):
                     if y2 >= row >= y1 and x2 >= col >= x1:
-                        if self.lights[row][col] == True:
-                            self.lights[row][col] = False
+                        if self.lights[row,col] == True:
+                            self.lights[row,col] = False
                         else:
-                            self.lights[row][col] = True
+                            self.lights[row,col] = True
     
     def count(self): #count the number of lights which are on
         count = 0
         for row in range(len(self.lights)):
             for col in range(len(self.lights[row])):
-                if self.lights[row][col] == True:
+                if self.lights[row,col] == True:
                     count += 1
         return count
