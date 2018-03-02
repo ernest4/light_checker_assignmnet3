f11919a09c8ef1532cab886af6bfda904aa547d7
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2018-03-02 16:39:55 +0000

    Finished the fileParser. Can now take URLs as input as well as local files.

--- light_counter/fileParser.py
+++ light_counter/fileParser.py
@@ -1,6 +1,7 @@
 import re
 import sys
 from unittest import result
+import requests
 
 def fileParser(inputURI):
     '''
@@ -8,12 +9,36 @@ def fileParser(inputURI):
     '''
     pattern = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
     
+    size, instructions = None, []
+    
     if inputURI.startswith("http"):
         #process URL
-        return None, None
+        req = requests.get(inputURI).text
+        
+        returnedStringList = '\n'.join(req.split('\n')).splitlines()
+        
+        #print(returnedStringList[1:3])
+        
+        try:
+            size = int(returnedStringList[0])
+        except ValueError:
+            print("Input file "+inputURI+" has non integer size for the lights matrix.")
+            return None, None
+        
+        for line in returnedStringList[1:]:
+            result = pattern.match(line)
+            if result is not None:
+                result = list(result.groups())
+                #convert numbers to ints
+                for i in [1,2,3,4]:
+                    result[i] = int(result[i])
+                #turn negative values to zero
+                for i in [1,2,3,4]:
+                    if result[i] < 0:
+                        result[i] = 0
+                instructions.append(result)
     else:
         #process local file
-        size, instructions = None, []
         with open(inputURI, 'r') as inputFile:
             try:
                 size = int(inputFile.readline())
@@ -26,26 +51,13 @@ def fileParser(inputURI):
                     result = list(result.groups())
                     #convert numbers to ints
                     for i in [1,2,3,4]:
-                        print("iterator",i)
                         result[i] = int(result[i])
                     #turn negative values to zero
                     for i in [1,2,3,4]:
                         if result[i] < 0:
                             result[i] = 0
                     instructions.append(result)
-        
-        #for debugging
-        print(instructions)
-        print(len(instructions))
-        print(instructions[0][0])
-        print(instructions[0][1])
-        print(instructions[0][2])
-        print(instructions[0][3])
-        print(instructions[0][4])
-        print(instructions[0])
-        print(instructions[1])
-        print(instructions[2])
                 
-        return size, instructions
+    return size, instructions
         
         
