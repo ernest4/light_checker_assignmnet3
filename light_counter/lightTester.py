6274cf36839be484e650d76412ecf4382faff5d7
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2018-03-03 03:13:18 +0000

    Massive improvement in performance. Double for loops in LightTester.apply() method have been replaced with numpy array slicing. This has reduced runtime from hours or days to seconds and minutes.

--- light_counter/lightTester.py
+++ light_counter/lightTester.py
@@ -13,30 +13,16 @@ class LightTester:
     lights = None #!!! PUBLIC for testing purposes, make PRIVATE once tests are done !!!
     
     def __init__(self, size):
-        #self.lights = [[False]*size for _ in range(size)]
         self.lights = np.array([[False]*size for _ in range(size)])
         
     def apply(self, command):
         action, x1, y1, x2, y2 = command[0], command[1], command[2], command[3], command[4]
-        #print(action,',',x1,',',y1,',',x2,',',y2) #Testing information, printing commands
         if command[0] == "turn on":
-            for row in range(len(self.lights)):
-                for col in range(len(self.lights[row])):
-                    if y2 >= row >= y1 and x2 >= col >= x1:
-                        self.lights[row,col] = True
+            self.lights[y1:y2+1,x1:x2+1,] = True
         elif command[0] == "turn off":
-            for row in range(len(self.lights)):
-                for col in range(len(self.lights[row])):
-                    if y2 >= row >= y1 and x2 >= col >= x1:
-                        self.lights[row,col] = False
+            self.lights[y1:y2+1,x1:x2+1,] = False
         else: #switch
-            for row in range(len(self.lights)):
-                for col in range(len(self.lights[row])):
-                    if y2 >= row >= y1 and x2 >= col >= x1:
-                        if self.lights[row,col] == True:
-                            self.lights[row,col] = False
-                        else:
-                            self.lights[row,col] = True
+            self.lights[y1:y2+1,x1:x2+1] = ~self.lights[y1:y2+1,x1:x2+1]
     
     def count(self): #count the number of lights which are on
         count = 0
